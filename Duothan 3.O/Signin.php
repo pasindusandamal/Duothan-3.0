32ec7bbaa2de7bacf3b35bc2bc72efb1fa67e70d
Kavindu Sandaruwan <75980353+kavindu-sandaruwan@users.noreply.github.com>
2023-02-24 14:18:40 +0530

    Updated

--- Duothan 3.O/Signin.php
+++ Duothan 3.O/Signin.php
@@ -7,18 +7,18 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>Duthon 3.O</title>
 </head>
-<body style="color: blue; background-color: #102390cf">
-   <div class="container" style="width: 1000px; background-color: white;margin-top: 100px;border-style: groove;border-radius: 20px; border-width: 3px;border-color: yellow">
+<body style="color: blue; background-color: white">
+   <div class="container" style="width: 800px; background-color: white;margin-top: 150px;border-style: groove;border-radius: 20px; border-width: 3px;border-color: yellow">
     <div class="row">
         <div class="col">
-            <img src="./Img/loginbg.jpg" style="width: 550px; margin-left: -12px;border-radius: 20px;">
+            <img src="./Img/loginbg.jpg" style="width: 700px; margin-left: -12px;border-radius: 20px;">
         </div>
-        <div class="col" style="padding-top: 10%;">
+        <div class="col" style="margin-left:-300px">
         <form method="post" action="login.php">
 
                 <div class="mb-3">
                   
-                  <h2>Sign In</h2>
+                  <h2 style="text-align:center;margin-top:40px;">Sign In</h2>
                    
                   </div>
                   <br><br>
@@ -32,8 +32,8 @@
                   <input type="password" class="form-control"  placeholder="Passowrd" name="password">
                 </div>
                 
-                <button type="submit" class="btn btn-primary ">Sign in</button>
-                <center><p>Don't have an account?<a href="signup.html" style="text-decoration: none;">Sign up</a></p></center>
+                <button type="submit" class="btn btn-primary " style="width:100%;">Sign in</button><br><br>
+                <center><p>Don't have an account?<a href="signup.php" style="text-decoration: none;">Sign Up</a></p></center>
               </form>
         </div>
     </div>
